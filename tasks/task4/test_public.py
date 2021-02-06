6e034e44496cdfc38dd2f466732afda7437ea334
Ilia Kuzmin <kyzmin.ilia@yandex.ru>
2021-02-06 21:42:08 +0300

    added tests for fourth task

--- tasks/task4/test_public.py
+++ tasks/task4/test_public.py
@@ -5,7 +5,7 @@ from .task import task4
 
 class Case:
     def __init__(self, name: str, n: int, connects: list,
-                 critical_places: list):
+                 critical_places: set):
         self._name = name
         self.n = n
         self.connects = connects
@@ -22,7 +22,7 @@ TEST_CASES = [
         connects=[
             (5, [1, 2, 3, 4]),
         ],
-        critical_places=set([5]),
+        critical_places={5},
     ),
     Case(
         name='base2',
@@ -31,7 +31,46 @@ TEST_CASES = [
             (2, [1, 3]),
             (5, [4, 6, 2]),
         ],
-        critical_places=set([5, 2]),
+        critical_places={5, 2},
+    ),
+    Case(
+        name='base3',
+        n=6,
+        connects=[
+            (1, [2, 3]),
+            (4, [3, 6]),
+            (5, [3, 6])
+        ],
+        critical_places={1, 3},
+    ),
+    Case(
+        name='base4',
+        n=4,
+        connects=[
+            (2, [1, 3]),
+            (3, [4])
+        ],
+        critical_places={2, 3},
+    ),
+    Case(
+        name='base5',
+        n=9,
+        connects=[
+            (1, [2, 3, 9]),
+            (8, [6, 2]),
+            (5, [3, 6]),
+            (4, [3, 7])
+        ],
+        critical_places={1, 4, 3},
+    ),
+    Case(
+        name='base6',
+        n=3,
+        connects=[
+            (1, [2, 3]),
+            (2, [3])
+        ],
+        critical_places=set(),
     ),
 ]
 
